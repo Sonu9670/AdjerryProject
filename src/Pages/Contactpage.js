a548554ef243c238fe1537bbefd4ec67f39f3bc5
helodeepakji <helodeepakji@gmail.com>
2024-12-26 18:41:09 +0530

    css

--- src/Pages/Contactpage.js
+++ src/Pages/Contactpage.js
@@ -1,28 +1,32 @@
 import React from "react";
 import "./Contactpage.css";
+import Feq from './Feq';
 import Nice from './Assests/contactimg.jpeg'
 
 const Contactpage = () => {
   return (
-    <div className="section">
-      <div class="contact ">
-        <div class="my-id">
-          <h1>Contact Us</h1>
-          <input type="text" name="name" placeholder="Name" className="input" />
-          <br />
-          <input type="text" name="email" placeholder="Email" className="input" />
-          <br />
-          <input type="text" name="Phone" placeholder="Phone" className="input" />
-          <br />
-          <input type="text" name="Purpose" placeholder="Purpose" className="input" />
-          <br />
-          <button>Send</button>
-        </div>
-        <div class="one">
-          <img src={Nice}  />
+    <div>
+      <div className="section">
+        <div class="contact ">
+          <div class="my-id">
+            <h1>Contact Us</h1>
+            <input type="text" name="name" placeholder="Name" className="input" />
+            <br />
+            <input type="text" name="email" placeholder="Email" className="input" />
+            <br />
+            <input type="text" name="Phone" placeholder="Phone" className="input" />
+            <br />
+            <input type="text" name="Purpose" placeholder="Purpose" className="input" />
+            <br />
+            <button>Send</button>
+          </div>
+          <div class="one">
+            <img src={Nice} />
+          </div>
         </div>
       </div>
-    </div>
+      <Feq/>
+      </div>
   );
 };
 
